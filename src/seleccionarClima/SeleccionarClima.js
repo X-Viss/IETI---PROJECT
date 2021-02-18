d323da9201bec1c98f43517ff1a9f6ba06dfe514
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2021-02-18 12:54:48 -0500

    SeleccionarClima completo

--- src/seleccionarClima/SeleccionarClima.js
+++ src/seleccionarClima/SeleccionarClima.js
@@ -2,11 +2,8 @@ import React from 'react';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import "./SeleccionarClima"
-
 
 export class SeleccionarClima extends React.Component {
 
@@ -17,29 +14,42 @@ export class SeleccionarClima extends React.Component {
     render() {
 
         let imgs = [
-            'https://i.ibb.co/ssC1Wz1/KONICA-MINOLTA-DIGITAL-CAMERA.jpg',
-            'https://i.ibb.co/XsY2PSt/oto-o.jpg',
-            'https://i.ibb.co/rQ6K4Pp/primavera.jpg',
-            'https://i.ibb.co/8BHqHSC/verano.jpg'
+            {
+                path : 'https://i.ibb.co/ssC1Wz1/KONICA-MINOLTA-DIGITAL-CAMERA.jpg',
+                name : "Invierno"
+
+            },
+            {
+                path: 'https://i.ibb.co/XsY2PSt/oto-o.jpg',
+                name: "Otoño"
+            },
+            {
+                path: 'https://i.ibb.co/rQ6K4Pp/primavera.jpg',
+                name: "Primavera"
+            
+            },
+            {
+                path: 'https://i.ibb.co/8BHqHSC/verano.jpg',
+                name: "Verano"
+            }
         ];
 
         return (
             <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around',  overflow: 'hidden'}}>
             <GridList cellHeight={250} style={{width: 1500, height: 600}} >
-                <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
-                <ListSubheader component="div">Selecciona el clima</ListSubheader>
+                <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <h1>Escoge el clime</h1>
                 </GridListTile>
                 {imgs.map((img, i) => (
-                <GridListTile key={i}>
-                    <img src={img} style={{width: 800, height: 600}} />
+                <GridListTile key={i} >
+                    <img src={img.path} style={{width: 800, height: 600}} />
                     <GridListTileBar
-                    title={i}
-                    subtitle={<span>by: {i}</span>}
+                    title={img.name}
                     actionIcon={
                         <FormControlLabel
-                            control={<Checkbox  name={i} />}
-                            label="ome"
-                            color="primary"
+                            control={<Checkbox  name={i} style={{color: "white"}} />}
+                            label="Clic aquí"
+                            style={{color: "white"}}
                         />
                     }
                     />
