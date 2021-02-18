e632a4a749293e3d8744027bdd074a15fd01317f
alejovasquero <alejo_704@hotmail.com>
2021-02-18 13:22:36 -0500

    :sparkles: XV-20 Delete dialog

--- src/travel/TravelList.js
+++ src/travel/TravelList.js
@@ -1,6 +1,12 @@
 
 import React, { useDebugValue } from 'react';
 import TravelCard from './TravelCard.js';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 class TravelList extends React.Component {
 
@@ -8,7 +14,8 @@ class TravelList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: "rr",
+            currentCardDelete: 0,
+            deleteConfirmationDialogOpen: false,
             travels: [{
                 name: "Negocios de cartera",
                 place: "Argentina",
@@ -27,29 +34,60 @@ class TravelList extends React.Component {
         };
 
         this.handleCardDelete = this.handleCardDelete.bind(this);
+        this.handleDeleteCardConfirmation = this.handleDeleteCardConfirmation.bind(this);
+        this.handleDialogClose = this.handleDialogClose.bind(this);
     }
 
+    handleCardDelete(key) {
+        this.setState({currentCardDelete: key, deleteConfirmationDialogOpen: true});
+    }
 
-    handleCardDelete(key){
+    handleDialogClose(){
+        this.setState({deleteConfirmationDialogOpen: false});
+    }
+
+    handleDeleteCardConfirmation(){
         this.setState(prevState => {
-            const travels = prevState.travels.filter(item => item.key !== key);
+            const travels = prevState.travels.filter(item => item.key !== this.state.currentCardDelete);
             return {
                 travels
             }
-        });
+        }, this.handleDialogClose);
     }
 
     render() {
         const renderTravels = this.state.travels.map((value, index) => {
             value.key = index;
             return (
-            <TravelCard key= {index} keyName={index} name={value.name} place={value.place} date={value.date} onDelete={this.onCardDelete} onCardDelete={this.handleCardDelete}>
-            </TravelCard>
+                <TravelCard key={index} keyName={index} name={value.name} place={value.place} date={value.date} onDelete={this.onCardDelete} onCardDelete={this.handleCardDelete}>
+                </TravelCard>
             );
         })
         return (
             <div>
                 {renderTravels}
+
+                <Dialog
+                    open={this.state.deleteConfirmationDialogOpen}
+                    onClose={this.handleDialogClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Borrar viaje"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            ¿Estás seguro que deseas borrar este viaje?
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleDialogClose} color="primary">
+                            Cancelar
+                        </Button>
+                        <Button onClick={this.handleDeleteCardConfirmation} color="primary" autoFocus>
+                            Confirmar
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         )
     }
