0e3ec0c7705c767821f376c56542ea79083e8cfc
PaoCu <paola.cuellar@mail.escuelaing.edu.co>
2021-02-28 22:25:53 -0500

    :recycle:

--- src/components/AdvancedGridList.js
+++ src/components/AdvancedGridList.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from "@material-ui/icons/Info";
 import {Typography} from "@material-ui/core";
+import BannerGrid from "./BannerGrid";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -42,18 +40,7 @@ export default function AdvancedGridList(props) {
             <GridList cellHeight={200} spacing={1} cols={props.numcols ? props.numcols : 2} className={classes.gridList}>
                 {props.tileData.map((tile) => (
                     <GridListTile key={tile.img} cols={tile.featured} >
-                        <img src={tile.img} alt={tile.title} />
-                        {tile.title && tile.author ?
-                            <GridListTileBar
-                                title={tile.title}
-                                subtitle={<span>by: {tile.author}</span>}
-                                actionIcon={
-                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                                        <InfoIcon />
-                                    </IconButton>
-                                }
-                            /> : ""
-                        }
+                        <BannerGrid tile={tile} classes = {classes}/>
                     </GridListTile>
                 ))}
             </GridList>
--- src/components/SingleLineGridList.js
+++ src/components/SingleLineGridList.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from "@material-ui/icons/Info";
 import {Typography} from "@material-ui/core";
+import BannerGrid from "./BannerGrid";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -43,18 +41,8 @@ export default function SingleLineGridList(props) {
             <GridList className={classes.gridList} cols={2.5}>
                 {props.tileData.map((tile) => (
                     <GridListTile key={tile.img}>
-                        <img src={tile.img} alt={tile.title} />
-                        {tile.title && tile.author ?
-                            <GridListTileBar
-                                title={tile.title}
-                                subtitle={<span>by: {tile.author}</span>}
-                                actionIcon={
-                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
-                                        <InfoIcon />
-                                    </IconButton>
-                                }
-                            /> : ""
-                        }
+                        <BannerGrid tile={tile} classes = {classes}/>
+
                     </GridListTile>
                 ))}
             </GridList>
