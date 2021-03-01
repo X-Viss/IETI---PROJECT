24c6f4984a870686086d4c0f24978bc893c8783d
PaoCu <paola.cuellar@mail.escuelaing.edu.co>
2021-02-28 22:08:15 -0500

    :white_check_mark: :recycle:

---
+++ src/components/SingleLineGridList.test.js
@@ -0,0 +1,35 @@
+import {act, render, screen} from '@testing-library/react';
+import {unmountComponentAtNode} from "react-dom";
+import {datatest, datatest1} from "../tmp/datamock";
+import SingleLineGridList from "./SingleLineGridList";
+import React from "react";
+
+let container = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("Renderiza SingleLineGridList", () => {
+    act(() => {
+        render(<SingleLineGridList tileData={datatest} title={"tipos de viaje"} />, container);
+    });
+
+    const linkElement = screen.getByText(/tipos de viaje/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+it("Renderiza SingleLineGridList sin información de autor o título", () => {
+    act(() => {
+        render(<SingleLineGridList tileData={datatest1} title={"inspirate"} numcols={4} />, container);
+    });
+
+    const linkElement = screen.getByText(/inspirate/i);
+    expect(linkElement).toBeInTheDocument();
+});
