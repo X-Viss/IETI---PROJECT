b45d5a61dbae34cd58ad6f0cb257d05b15b750b9
Wasawsky <mjbc_20@hotmail.com>
2021-03-01 14:56:38 -0500

    Afinando presentacion preguntas, componente listo

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
-
-import Typography from '@material-ui/core/Typography';
+import logo from './logo.svg';
 import './App.css';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    overflow: 'hidden',
-    padding: theme.spacing(0, 3),
-  },
-  paper: {
-    margin: theme.spacing(1),
-    padding: theme.spacing(2),
-  },
-}));
-
-const questions = ["pregunta 1","pregunta2","pregunta3"];
-const answer = ["respuesta 1","respuesta2","respuesta3"];
+import QuestList from './components/QuestList.js';
 
 function App() {
-  const classes = useStyles();
   return (
-
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container direction="row"
-            justify="flex-start"
-            alignItems="center" spacing={5}
-            >
-          <Grid container xs={12}>
-            <Typography noWrap>{questions[0]}</Typography>
-          </Grid>
-          <Grid container xs={12}>
-            <Typography noWrap>{answer[0]}</Typography>
-          </Grid>
-        </Grid>
-      </Paper>
+    <div className="App">
+      <QuestList></QuestList>
     </div>
   );
 }
--- src/components/quest.js
+++ src/components/quest.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import 'react-datepicker/dist/react-datepicker.css';
+import { withStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+const styles = (theme) => ({
+  root: {
+    padding: theme.spacing(0, 3),
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+})
 
-export class Quest extends React.Component {
+class Quest extends React.Component {
     constructor(props) {
       super(props);
-        this.state = {text:''};
     }
   
     render() {
+        const {classes} = this.props;
         return (
-            <div>
-                <a>Preguntas</a>
+            <div className={classes.root}>
+                <Accordion>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography className={classes.heading}>{this.props.qry}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        {this.props.ans}
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
             </div>
         );
     }
-  
   }
+
+  export default withStyles(styles)(Quest);
