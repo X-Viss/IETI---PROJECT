c23e128670ed32659c2d919928ce0334fcce5752
alejovasquero <alejo_704@hotmail.com>
2021-03-02 01:21:49 -0500

    :bug: XV-5 fix tests

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
-import Travels from './travel'
-import { createMuiTheme, makeStyles, MuiThemeProvider } from '@material-ui/core';
-
-const THEME = createMuiTheme({
-  typography: {
-    "fontFamily": "Helvetica",
-    "allVariants": {
-      color: "white"
-    },
-    "fontSize": "auto",
-    "fontWeightLight": 300,
-    "fontWeightRegular": 400,
-    "fontWeightMedium": 500,
-    "useNextVariants": true
-  }
-});
 
 function App() {
-
   return (
-    <div className="App" >
-      <MuiThemeProvider theme={THEME}>
-        <Travels></Travels>
-      </MuiThemeProvider>
-
-
-    </div >
+    <div className="App">
+      
+    </div>
   );
 }
 
