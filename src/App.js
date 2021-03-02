ef72765dc9eedc1a6f209afde14324e20981c403
alejovasquero <alejo_704@hotmail.com>
2021-03-02 01:15:32 -0500

    :bug: App.js fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
-import Travels from './travel'
-import { createMuiTheme, makeStyles, MuiThemeProvider } from '@material-ui/core';
-
-const THEME = createMuiTheme({
-  typography: {
-    "fontFamily": "Helvetica",
-    "fontSize": "auto",
-    "fontWeightLight": 300,
-    "fontWeightRegular": 400,
-    "fontWeightMedium": 500,
-    "useNextVariants": true
-  }
-});
+import Main from './seleccionarClima/decoracionMain.js';
 
 function App() {
-
   return (
-    <div className="App" >
-      learn react
-      <MuiThemeProvider theme={THEME}>
-        <Travels></Travels>
-      </MuiThemeProvider>
+    <div className="App">
 
-
-    </div >
+    </div>
   );
 }
 
