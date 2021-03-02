b6d24144f0f0f60586869509a2b676ed8cd7badb
Lfersar <luisafsar@gmail.com>
2021-03-02 01:59:31 -0500

    :lipstick: css login

--- src/form/components.js
+++ src/form/components.js
@@ -2,6 +2,7 @@ import React from "react"
 import { MapArticles } from './articles'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button';
+import './login.css';
 
 function itemsForLoginForm(){
     return[
@@ -22,10 +23,25 @@ export function LoginForm(props){
         event.preventDefault()
         console.log('submit')
     }
-    return <form onSubmit={handleSubmit}>
-        <center>
-            <MapArticles items={itemsForLoginForm()}/>
-            <Button type="submit" variant="contained">Login</Button>
-        </center>
-    </form>
+    return <div class="container">
+        <div class= "sing-in">
+            <div class="sign-in-container">
+                <form onSubmit={handleSubmit}>
+                    <center>
+                    <h1>Sign in</h1>
+                    <p/>
+                    <MapArticles items={itemsForLoginForm()}/>
+                    <p/>
+                    <Button type="submit" variant="contained">Login</Button>
+                    <p/><p/>
+                    </center>
+                    <Button type="submit">No tienes cuenta? Registrate</Button>
+                </form>
+            </div>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+            </div>
+	    </div>
+    </div>
 }
