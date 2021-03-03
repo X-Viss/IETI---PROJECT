fc8ecaf941d2ccaba5855fc76d378ec4df28f2ab
Wasawsky <mjbc_20@hotmail.com>
2021-03-03 00:35:22 -0500

    fix

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
