89f1db8ce80c948718e1991b4d4d6eed5222125f
Lfersar <luisafsar@gmail.com>
2021-03-03 05:15:48 -0500

    :white_check_mark: test

---
+++ src/form/404.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './404.css';
+
+export function NotFoundForm(props) {
+    return <div>
+        <aside>
+            <img src="https://image.freepik.com/free-vector/error-404-found-glitch-effect_8024-4.jpg" alt="404 Image" />
+        </aside>
+        <main>
+            <h1>Oopss..... !</h1>
+            <p>
+                Lo lamentamos parece que te has perdido Página no encontrada 
+            </p>
+        </main>
+    </div>
+}
