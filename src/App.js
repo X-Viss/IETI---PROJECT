93278427c605b2e983355364050863c4a7c98cb9
alejovasquero <alejo_704@hotmail.com>
2021-03-02 21:33:16 -0500

    :fix: XV-8 Merge fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import  TravelList from './travel/TravelList.js';
-import Travels from './travel'
+
 function App() {
   return (
     <div className="App">
-      learn react
-      <Travels></Travels>
+      
     </div>
   );
 }
