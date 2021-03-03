46b8ad735f7b8a9e50eebca9f3507e6b7e91a4bc
Wasawsky <mjbc_20@hotmail.com>
2021-03-02 22:10:58 -0500

    agregando categoria personal

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import AddIcon from '@material-ui/icons/Add';
 import IconButton from '@material-ui/core/IconButton';
+import TabIcon from '@material-ui/icons/Tab';
 
 const styles = (theme) => ({
   root: {
@@ -45,6 +45,10 @@ class NewItem extends React.Component {
                     <Grid container justify="flex-start" alignItems="center" spacing={0}>
                           <IconButton item aria-label="edit item" onClick={this.onSubForm}><AddIcon /></IconButton>
                           <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </Grid>
+                    <Grid container justify="flex-end" alignItems="center" spacing={0}>
+                      <layer>Agregar Categoria Personal</layer>
+                    <IconButton item aria-label="edit item"><TabIcon /></IconButton>
                     </Grid>
                 </CardContent>
             </Card>
