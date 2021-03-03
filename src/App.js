e019db83c848fb561517b0cb79fa7abbe105766b
Wasawsky <mjbc_20@hotmail.com>
2021-03-02 23:57:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Categoria from './components/Categoria.js';
 
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Categoria></Categoria>
+
     </div>
   );
 }
