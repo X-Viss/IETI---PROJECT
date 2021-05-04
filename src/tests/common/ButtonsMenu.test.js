71e94c3f05bd7669057f8432cd98dd6846633191
sebastian nieto <juan.nieto-mo@mail.escuelaing.edu.co>
2021-05-04 12:25:43 -0500

    correccion pruebas

--- src/tests/common/ButtonsMenu.test.js
+++ src/tests/common/ButtonsMenu.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import ButtonsMenu from '../../common/ButtonsMenu';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 test('renders buttons with text', () => {
-    render(<ButtonsMenu />);
+    render(
+        <Router>
+            <ButtonsMenu />
+        </Router>
+    );
     const preguntas = screen.getByText(/Preguntas Frecuentes/i);
     const anadir = screen.getByText(/Preguntas Frecuentes/i);
 
